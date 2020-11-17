5b341766ef02fd1ca5aea6f21074506b8cd16510
Ralph Langit <ralphrebel7@gmail.com>
2020-11-16 19:13:22 -0800

    my first commit

---
+++ Topic 1/Week 1/index.js
@@ -0,0 +1,22 @@
+var itemPrice = 5;
+var walletMoney = 10;
+var friends = 0;
+var age = 25;
+var firstName = "Ralph";
+var lastName = "Langit";
+var middleInitial = "P";
+
+var newWalletAmount = walletMoney - itemPrice;
+var numberOfFriends = 2*age + friends;
+var fullName = firstName + " " + middleInitial + " " + lastName;
+
+console.log("This Rubiks cube cost " + "$" + itemPrice);
+console.log("I had " + "$" + walletMoney);
+console.log("I had " + friends + " friends");
+console.log("I am " + age + " years old");
+console.log("My first name is " + firstName);
+console.log("My last name is " + lastName);
+console.log("My middle initial is " + middleInitial);
+console.log("After my purchase, my wallet currently has " + "$" + newWalletAmount);
+console.log("I have " + numberOfFriends + " friends");
+console.log("My name is " + fullName);
