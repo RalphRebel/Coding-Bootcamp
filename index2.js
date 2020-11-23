4624e516eda4989895ee714bed5c5ea4f35d156b
Ralph Langit <41921443+RalphRebel@users.noreply.github.com>
2020-11-23 11:34:16 -0800

    Week 2

---
+++ index2.js
@@ -0,0 +1,20 @@
+var a = true && false; 
+    console.log(a);
+var b = true || false; 
+    console.log(b);
+var c = false && false;
+    console.log(c);
+var d = true && (false || true);
+    console.log(d);
+var e =false || (true && false);
+    console.log(e);
+var f = (false | 1< 5);
+    console.log(f);
+var g = 5 >= 4 && 1 > 3;
+    console.log(g);
+var h = 10 < 4 || 1> 4
+    console.log(h);
+var i = 12 >= 2 && 1 < 24;
+    console.log(i);
+var j = 'Hello'.charAt(0) == 'h';
+    console.log(j);
